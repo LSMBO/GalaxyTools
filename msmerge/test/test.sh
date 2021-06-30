85405ed924d060565f8e5ddb085f74312994e7db
AlexandreBurel <alexandre.burel@unistra.fr>
2021-06-30 09:44:17 +0200

    Changed permission and output file name in tests

--- msmerge/test/test.sh
+++ msmerge/test/test.sh
@@ -1,1 +1,1 @@
-../msmerge.pl test.mgf ./E040566.mgf E040566.mgf ./E040676.mgf E040676.mgf ./E040750.mgf E040750.mgf ./E040777.mgf E040777.mgf
+../msmerge.pl output.mgf ./E040566.mgf E040566.mgf ./E040676.mgf E040676.mgf ./E040750.mgf E040750.mgf ./E040777.mgf E040777.mgf
